c14d8ca50b88cef65bb83ecee0d0e287cf5f82f2
vickymalindo <vicky.malindo@gmail.com>
2022-07-31 00:45:43 +0700

    add validation route

--- src/components/user/Login.jsx
+++ src/components/user/Login.jsx
@@ -32,34 +32,35 @@ const Login = () => {
     return (
         <>
         <Nav />
-            {loginStatus ? <Navigate to='/user' />
+            {loginStatus ? 
+                <Navigate to='/user' />
             :
-            <div className="w-full h-screen flex flex-col items-center justify-center px-5 md:px-0">
-                <h1 className={errorMessage ? 'block py-1 px-2 bg-red-300 text-red-600 border-none rounded-lg mb-1' : 'hidden'}>{errorMessage}</h1>
-                <form className="w-full md:w-1/3 bg-white rounded-lg border border-gray-500" onSubmit={handleSubmit}>
-                    <div className="flex font-bold justify-center mt-6">
-                        <img className="h-20 w-20" src="https://raw.githubusercontent.com/sefyudem/Responsive-Login-Form/master/img/avatar.svg" alt='gambar' />
-                    </div>
-                    <h2 className="text-3xl text-center text-gray-700 mb-4">Login Form</h2>
-                    <div className="px-12 pb-10">
-                        <div className="w-full mb-2">
-                            <div className="flex items-center">
-                                <input type='text' placeholder="Username"
-                                    className="m-auto px-8 w-full border border-gray-500 rounded py-2 text-gray-700 focus:outline-none" onChange={(e) => setUsername(e.target.value)} />
-                            </div>
+                <div className="w-full h-screen flex flex-col items-center justify-center px-5 md:px-0">
+                    <h1 className={errorMessage ? 'block py-1 px-2 bg-red-300 text-red-600 border-none rounded-lg mb-1' : 'hidden'}>{errorMessage}</h1>
+                    <form className="w-full md:w-1/3 bg-white rounded-lg border border-gray-500" onSubmit={handleSubmit}>
+                        <div className="flex font-bold justify-center mt-6">
+                            <img className="h-20 w-20" src="https://raw.githubusercontent.com/sefyudem/Responsive-Login-Form/master/img/avatar.svg" alt='gambar' />
                         </div>
-                        <div className="w-full mb-2">
-                            <div className="flex items-center">
-                                <input type='password' placeholder="Password"
-                                    className="m-auto px-8 w-full border border-gray-500 rounded py-2 text-gray-700 focus:outline-none" onChange={(e) => setPassword(e.target.value)} />
+                        <h2 className="text-3xl text-center text-gray-700 mb-4">Login Form</h2>
+                        <div className="px-12 pb-10">
+                            <div className="w-full mb-2">
+                                <div className="flex items-center">
+                                    <input type='text' placeholder="Username"
+                                        className="m-auto px-8 w-full border border-gray-500 rounded py-2 text-gray-700 focus:outline-none" onChange={(e) => setUsername(e.target.value)} />
+                                </div>
                             </div>
+                            <div className="w-full mb-2">
+                                <div className="flex items-center">
+                                    <input type='password' placeholder="Password"
+                                        className="m-auto px-8 w-full border border-gray-500 rounded py-2 text-gray-700 focus:outline-none" onChange={(e) => setPassword(e.target.value)} />
+                                </div>
+                            </div>
+                            <button type="submit"
+                                className="w-full py-2 rounded-full bg-green-300 text-green-900 focus:outline-none">Submit
+                            </button>
                         </div>
-                        <button type="submit"
-                            className="w-full py-2 rounded-full bg-green-300 text-green-900 focus:outline-none">Submit
-                        </button>
-                    </div>
-                </form>
-            </div> 
+                    </form>
+                </div> 
             }
         <Footer />
         </>
--- src/components/user/Main.jsx
+++ src/components/user/Main.jsx
@@ -3,7 +3,7 @@ import React, {useState, useEffect} from 'react'
 import { FaStar } from 'react-icons/fa';
 import Footer from '../front/Footer';
 import Nav from '../front/Nav'
-import { useNavigate } from 'react-router-dom';
+import { Navigate, useNavigate } from 'react-router-dom';
 
 const Main = () => {
   const [allProduct, setAllProduct] = useState([]);
@@ -28,34 +28,47 @@ const Main = () => {
   //   return <div>...Loading</div>
   // }
 
-  // const token = localStorage.getItem('token')
+  const token = localStorage.getItem('token')
   // if(!token){
   //   navigate('/login')
   // }
   
   return (
     <div>
-      <Nav />
-      <div className='grid grid-cols-2 md:grid-cols-5 items-center justify-items-stretch gap-1 md:gap-3 p-6 md:p-12'>
-        {allProduct.map(val => {
-          return (
-            <div className="border border-gray-500 border-solid rounded-md shadow-lg px-1.5 py-1 w-36 md:px-3 md:py-2 md:w-48" key={val.id}>
-              <div className='w-full border-none rounded overflow-hidden aspect-square m-auto'>
-                <img src={val.image} alt="gambar" className='m-auto object-cover object-center' />
-              </div>
-                <p className='w-full overflow-hidden whitespace-nowrap text-ellipsis'>{val.title}</p>
-                <p className='text-sm md:text-base'>$ {val.price}</p>
-                <div className='flex items-center '>
-                  <FaStar className='text-sm md:text-base text-yellow-400'/>
-                  <p className='text-sm md:text-base ml-1'>{val.rating.rate}</p>
-                </div>
-                  <p className='text-sm md:text-base'>Stock : {val.rating.count}</p>
-                  <button className='bg-green-300 text-green-900 px-2 py-1 rounded-md font-semibold text-md hover:scale-95' onClick={() => buyProduct(val.id)}>buy now</button>
+      {
+        token ? 
+        <div>
+          <Nav />
+          {allProduct.length === 0 ? 
+            <div className='w-full h-screen flex justify-center items-center'>
+              <div className='p-3 bg-green-300 text-green-900'>...loading</div>
+            </div> 
+          : 
+            <div className='grid grid-cols-2 md:grid-cols-5 items-center justify-items-stretch gap-1 md:gap-3 p-6 md:p-12'>
+              {allProduct.map(val => {
+                return (
+                  <div className="border border-gray-500 border-solid rounded-md shadow-lg px-1.5 py-1 w-36 md:px-3 md:py-2 md:w-48" key={val.id}>
+                    <div className='w-full border-none rounded overflow-hidden aspect-square m-auto'>
+                      <img src={val.image} alt="gambar" className='m-auto object-cover object-center' />
+                    </div>
+                      <p className='w-full overflow-hidden whitespace-nowrap text-ellipsis'>{val.title}</p>
+                      <p className='text-sm md:text-base'>$ {val.price}</p>
+                      <div className='flex items-center '>
+                        <FaStar className='text-sm md:text-base text-yellow-400'/>
+                        <p className='text-sm md:text-base ml-1'>{val.rating.rate}</p>
+                      </div>
+                        <p className='text-sm md:text-base'>Stock : {val.rating.count}</p>
+                        <button className='bg-green-300 text-green-900 px-2 py-1 rounded-md font-semibold text-md hover:scale-95' onClick={() => buyProduct(val.id)}>buy now</button>
+                  </div>
+                  )})
+              }
             </div>
-            )})
-        }
-        </div>
-        <Footer />
+          }
+          <Footer />
+        </div> : <Navigate to='/login'/>
+      }
+      
+        
     </div>
   )
 }
